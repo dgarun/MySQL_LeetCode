cf22fa1072eed9a68807318e97982646297aba5c
Arunkumar <dgarun486@gmail.com>
2022-03-06 00:30:14 +0900

    Solved Merge two tables

---
+++ db_practice_Queries.sql
@@ -0,0 +1,24 @@
+#DDL Queries
+/*
+Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255));
+Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255));
+Truncate table Person;
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen');
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob');
+Truncate table Address;
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York');
+insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California');
+*/
+
+#Merge Tables
+select p.firstName,p.lastName,a.city,a.state from Person p left join Address a on p.personId = a.personId;
+
+#DDL - Employees Earning Higher than their Managers
+/*
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
+Truncate table Employee;
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', null);
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', null);
+*/
